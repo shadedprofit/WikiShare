87cd5330d9cacb534419600371e48c440bf904de
Edgar Pabon <edgar.pabon@gmail.com>
2015-06-28 12:17:22 -0400

    User can add and remove collaborators

--- app/controllers/collaborators_controller.rb
+++ app/controllers/collaborators_controller.rb
@@ -3,34 +3,54 @@ class CollaboratorsController < ApplicationController
     @wiki = Wiki.find(params[:wiki_id])
     @collaborators = @wiki.collaborators
     @users = User.all
+    @users.each do |user|
+      @non_collaborators = []
+      @non_collaborators << user
+    end
     authorize @wiki
   end
 
   def create
     @wiki = Wiki.find(params[:wiki_id])
-
     params[:user_id].each_key do |user_id|
       user = User.find(user_id)
-      @wiki.collaborators.create(:user => user)
+      @collaborator = @wiki.collaborators.create(:user => user)
     end
+    @users = User.all
+    @non_collaborators = User.all
+    @collaborator.wiki = @wiki
+    @collaborators = @wiki.collaborators
     
-    if @wiki.save
-      redirect_to [@wiki, @collaborators]
-    end
-  end
-
-  def update
-    @wiki = Wiki.find(params[:wiki_id])
-
-    params[:wiki][:user_ids].each do |user_id|
-      user = User.find(user_id)
-      @wiki.collaborators.create(:user => user)
+    if @collaborator.save
+      flash[:notice] = "Collaborator was added successfully."
+      render :index
+    else
+      flash[:error] = "There was an error. Please try again."
+      render :index
     end
 
+    # respond_to do |format|
+    #   format.html
+    #   format.js
+    # end
   end
 
   def destroy
-
+    @wiki = Wiki.find(params[:wiki_id])
+    @collaborator = @wiki.collaborators.find(params[:id])
+    authorize @wiki
+    @users = User.all
+    
+    if @collaborator.destroy
+      flash[:notice] = "Collaborator was removed."
+      redirect_to :back
+    else
+      flash[:error] = "There was an error removing the collaborator. Please try again."
+      render :index
+    end
+    
   end
+
+ 
 
 end
--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -28,17 +28,8 @@ class WikisController < ApplicationController
     @user = current_user
     @wiki = current_user.wikis.build(wiki_params)
     @wiki.user = @user
-
-    # puts params[:wiki][:user_ids]
     
     authorize @wiki
-
-    # @wiki.save
-
-    # params[:wiki][:user_ids].each do |user_id|
-    #   user = User.find(user_id)
-    #   @wiki.collaborators.create(:user => user)
-    # end
 
     if @wiki.save
       flash[:notice] = "Wiki was saved."
@@ -51,17 +42,8 @@ class WikisController < ApplicationController
 
   def update
     @wiki = Wiki.find(params[:id])
-
-    puts params[:wiki][:user_ids]
-
     authorize @wiki
-    
     @wiki.save
-
-    params[:wiki][:user_ids].each do |user_id|
-      user = User.find(user_id)
-      @wiki.collaborators.create(:user => user)
-    end
 
     if @wiki.update_attributes(wiki_params)
       flash[:notice] = "Wiki was updated."
@@ -75,14 +57,16 @@ class WikisController < ApplicationController
   def destroy
     @user = current_user
     @wiki = Wiki.find(params[:id])
-    title = @wiki.title
     authorize @wiki
-    @wiki.destroy
-
-    respond_to do |format|
-      format.html
-      format.js
+    
+    if @wiki.destroy
+      flash[:notice] = "Wiki was deleted."
+      render :index
+    else
+      flash[:error] = "There was an error deleting the wiki. Please try again."
+      render :edit
     end
+
   end
 
   private
